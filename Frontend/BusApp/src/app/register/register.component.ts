1fcb8eb416c9542ddda499f7952c637c3286844b
Milica <samardzija.ra66.2018@uns.ac.rs>
2022-08-29 19:19:57 +0200

    feat: register be

--- Frontend/BusApp/src/app/register/register.component.ts
+++ Frontend/BusApp/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+import { AuthService } from '../login/auth.service';
+import { User } from '../model/User';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  user: User = {name: "", surname: "", telephone: "", email: "", password: "", role: "", country: "", city: "", street: "", number: ""}
 
-  constructor() { }
+  constructor(private authService : AuthService, public dialogRef: MatDialogRef<RegisterComponent>) { }
 
   ngOnInit(): void {
   }
 
+  signUp(){
+    this.authService.register(this.user).subscribe(
+      response => {
+        console.log(response);
+      }
+    );
+    this.dialogRef.close();
+  }
+
+  cancel(){
+    this.dialogRef.close();
+  }
 }
--- Frontend/BusApp/src/app/welcome/welcome.component.ts
+++ Frontend/BusApp/src/app/welcome/welcome.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { LoginComponent } from '../login/login.component';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-welcome',
@@ -18,6 +19,13 @@ export class WelcomeComponent implements OnInit {
     const dialogRef = this.dialog.open(LoginComponent, {
       width: '560px',
       height: '370px',
+    }); 
+  }
+
+  signUp(){
+    const dialogRef = this.dialog.open(RegisterComponent, {
+      width: '700px',
+      height: '680px',
     }); 
   }
 }
